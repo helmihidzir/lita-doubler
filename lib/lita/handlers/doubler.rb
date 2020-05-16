2431e8c1833861509aa9787fdbde97c7d699f2a7
helmihidzir <helmihidzir@gmail.com>
2020-05-16 14:17:51 +0800

    Initial commit

---
+++ lib/lita/handlers/doubler.rb
@@ -0,0 +1,24 @@
+module Lita
+  module Handlers
+    class Doubler < Handler
+      route(
+          /^double\s+(\d+)$/i,
+          :respond_with_double,
+          command: true, help: { 'double N' => 'prints N + N' }
+      )
+
+      def double_number(n)
+        n + n
+      end
+
+      def respond_with_double(response)
+        n = response.match_data.captures.first
+        n = Integer(n)
+
+        response.reply "#{n} + #{n} = #{double_number n}"
+      end
+
+      Lita.register_handler(self)
+    end
+  end
+end
